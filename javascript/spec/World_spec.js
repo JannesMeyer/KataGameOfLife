2c48ab29ae280e1c0d971b6b288e7aa15223fba8
Jannes <jannes.meyer@gmail.com>
2015-04-27 13:30:03 +0200

    Change tabs to spaces

--- javascript/spec/World_spec.js
+++ javascript/spec/World_spec.js
@@ -3,43 +3,43 @@ var Cell = require('../src/Cell.js');
 
 describe("World", function() {
 
-	var world;
+  var world;
 
-	beforeEach(function() {
-		world = new World(3, 3);
-	});
+  beforeEach(function() {
+    world = new World(3, 3);
+  });
 
-	it("returns a cell inside the bounds", function() {
-		expect(world.getCell(2, 2)).toEqual(jasmine.any(Cell));
-	});
+  it("returns a cell inside the bounds", function() {
+    expect(world.getCell(2, 2)).toEqual(jasmine.any(Cell));
+  });
 
-	it("doesn't return a cell outside the bounds", function() {
-		expect(world.getCell(-1, -1)).toBeUndefined();
-		expect(world.getCell( 3,  3)).toBeUndefined();
-	});
+  it("doesn't return a cell outside the bounds", function() {
+    expect(world.getCell(-1, -1)).toBeUndefined();
+    expect(world.getCell( 3,  3)).toBeUndefined();
+  });
 
-	it("has up to eight neighbors per cell", function() {
-		expect(world.getNeighbours(world.getCell(1, 1)).length).toBeLessThan(9);
-	});
+  it("has up to eight neighbors per cell", function() {
+    expect(world.getNeighbours(world.getCell(1, 1)).length).toBeLessThan(9);
+  });
 
-	it("pretty-prints the current state", function() {
-		world.getCell(1, 1).alive = true;
-		expect(world.toString()).toBe('Generation 1:\n3 3\n...\n.*.\n...');
-	});
+  it("pretty-prints the current state", function() {
+    world.getCell(1, 1).alive = true;
+    expect(world.toString()).toBe('Generation 1:\n3 3\n...\n.*.\n...');
+  });
 
-	it("loads the state from text and calculates the next generation", function() {
-		var world = World.fromString('Generation 1:\n\
+  it("loads the state from text and calculates the next generation", function() {
+    var world = World.fromString('Generation 1:\n\
 4 8\n\
 ........\n\
 ....*...\n\
 ...**...\n\
 ........').next();
-		expect(world.toString()).toBe('Generation 2:\n\
+    expect(world.toString()).toBe('Generation 2:\n\
 4 8\n\
 ........\n\
 ...**...\n\
 ...**...\n\
 ........');
-	});
+  });
 
 });
